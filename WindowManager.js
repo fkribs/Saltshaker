2ebc18708558a274f336a62f70ed0c5708be046b
Forrest Kribs <forrestkribs@comcast.net>
2025-11-01 19:46:52 -0600

    Change url to new deployed app

--- WindowManager.js
+++ WindowManager.js
@@ -2,37 +2,37 @@ const { BrowserWindow } = require('electron');
 const path = require('path');
 
 class WindowManager {
-    constructor() {
-        this.window = null;
-    }
+  constructor() {
+    this.window = null;
+  }
 
-    createWindow() {
-        this.window = new BrowserWindow({
-            width: 800,
-            height: 800,
-            show: true,
-            alwaysOnTop: true,
-            transparent: false,
-            backgroundColor: '#ffffff',
-            icon: path.join(__dirname, 'src', 'assets', 'icon.png'),
-            title: 'Salt Shaker',
-            webPreferences: {
-                preload: path.join(__dirname, 'preload.js'),
-                nodeIntegration: true,
-                contextIsolation: true,
-            },
-        });
+  createWindow() {
+    this.window = new BrowserWindow({
+      width: 800,
+      height: 800,
+      show: true,
+      alwaysOnTop: true,
+      transparent: false,
+      backgroundColor: '#ffffff',
+      icon: path.join(__dirname, 'src', 'assets', 'icon.png'),
+      title: 'Salt Shaker',
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: true,
+        contextIsolation: true,
+      },
+    });
 
-        this.window.loadURL(`https://localhost:7287/`);
+    this.window.loadURL(`https://signalrcorewebrtc20240210154024-cbhbe3c2c7a0a4hr.canadacentral-01.azurewebsites.net/`);
 
-        this.window.on('closed', () => {
-            this.window = null;
-        });
-    }
+    this.window.on('closed', () => {
+      this.window = null;
+    });
+  }
 
-    getWebContents() {
-        return this.window ? this.window.webContents : null;
-    }
+  getWebContents() {
+    return this.window ? this.window.webContents : null;
+  }
 }
 
 module.exports = WindowManager;
